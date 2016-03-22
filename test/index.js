aea6228b33cc1f7a7f757b52f2f5009f658af621
Luca Barone <luca.b@docsity.com>
2016-03-22 11:47:42 +0100

    test init

---
+++ test/index.js
@@ -0,0 +1,10 @@
+import tape from 'tape'
+
+tape('timing test', t => {
+	t.plan(2)
+
+	t.equal(typeof Date.now, 'function')
+
+	const start = Date.now()
+	setTimeout(() => t.equal(Date.now() - start, 100), 100)
+})
